5965864b3124ea5e28466f58c43b0c4957b9c51c
white4206 <1834893128@qq.com>
2025-05-30 15:53:35 +0800

    style: 清除vite默认案例,配置自定义主题
    desc:
    - 添加自定义主题, 包括黑暗模式
    - 添加AppBar color参数

---
+++ src/theme/index.ts
@@ -0,0 +1,75 @@
+import { createTheme } from "@mui/material/styles";
+
+// 增加调色板参数，加入wMain主题色
+declare module "@mui/material/styles" {
+  interface Palette {
+    wMain: Palette["primary"];
+    appBarColor: string;
+  }
+
+  interface PaletteOptions {
+    wMain?: PaletteOptions["primary"];
+    appBarColor?: string;
+  }
+}
+
+// 更新badge,Chip的颜色选项，使其包含wMain主题色选项
+declare module "@mui/material/Badge" {
+  interface BadgePropsColorOverrides {
+    wMain: true;
+  }
+}
+declare module "@mui/material/Chip" {
+  interface ChipPropsColorOverrides {
+    wMain: true;
+  }
+}
+declare module "@mui/material/AppBar" {
+  interface AppBarPropsColorOverrides {
+    appBarColor: true;
+  }
+}
+
+let lightTheme = createTheme({});
+lightTheme = createTheme(lightTheme, {
+  // 用augmentColor创建的自定义主题色
+  palette: {
+    primary: lightTheme.palette.augmentColor({
+      color: {
+        main: "#9286d1",
+      },
+      name: "wMain",
+    }),
+    wMain: lightTheme.palette.augmentColor({
+      color: {
+        main: "#9286d1",
+      },
+      name: "wMain",
+    }),
+    appBarColor: "#ffffff",
+  },
+});
+let darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+darkTheme = createTheme(darkTheme, {
+  // 用augmentColor创建的自定义主题色
+  palette: {
+    primary: darkTheme.palette.augmentColor({
+      color: {
+        main: "#9286d1",
+      },
+      name: "wMain",
+    }),
+    wMain: darkTheme.palette.augmentColor({
+      color: {
+        main: "#9286d1",
+      },
+      name: "wMain",
+    }),
+    appBarColor: "#000000",
+  },
+});
+export { lightTheme, darkTheme };

0daa8b736485cf161b95ce74a8ba4a76cf3a6b2b
white4206 <1834893128@qq.com>
2025-06-05 01:04:48 +0800

    refactor: 重构代码格式

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import './i18n'; // i18国际化
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
-)
+    <BrowserRouter>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </BrowserRouter>
+);
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,69 +1,69 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
 // 增加调色板参数，加入wMain主题色
-declare module "@mui/material/styles" {
-  interface Palette {
-    wMain: Palette["primary"];
-    appBarColor: string;
-  }
+declare module '@mui/material/styles' {
+    interface Palette {
+        wMain: Palette['primary'];
+        appBarColor: string;
+    }
 
-  interface PaletteOptions {
-    wMain?: PaletteOptions["primary"];
-    appBarColor?: string;
-  }
+    interface PaletteOptions {
+        wMain?: PaletteOptions['primary'];
+        appBarColor?: string;
+    }
 }
 
 // 更新badge,Chip的颜色选项，使其包含wMain主题色选项
-declare module "@mui/material/Badge" {
-  interface BadgePropsColorOverrides {
-    wMain: true;
-  }
+declare module '@mui/material/Badge' {
+    interface BadgePropsColorOverrides {
+        wMain: true;
+    }
 }
-declare module "@mui/material/Chip" {
-  interface ChipPropsColorOverrides {
-    wMain: true;
-  }
+declare module '@mui/material/Chip' {
+    interface ChipPropsColorOverrides {
+        wMain: true;
+    }
 }
 
 let lightTheme = createTheme({});
 lightTheme = createTheme(lightTheme, {
-  // 用augmentColor创建的自定义主题色
-  palette: {
-    primary: lightTheme.palette.augmentColor({
-      color: {
-        main: "#9286d1",
-      },
-      name: "wMain",
-    }),
-    wMain: lightTheme.palette.augmentColor({
-      color: {
-        main: "#9286d1",
-      },
-      name: "wMain",
-    }),
-    appBarColor: "#ffffff",
-  },
+    // 用augmentColor创建的自定义主题色
+    palette: {
+        primary: lightTheme.palette.augmentColor({
+            color: {
+                main: '#9286d1',
+            },
+            name: 'wMain',
+        }),
+        wMain: lightTheme.palette.augmentColor({
+            color: {
+                main: '#9286d1',
+            },
+            name: 'wMain',
+        }),
+        appBarColor: '#ffffff',
+    },
 });
 let darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
+    palette: {
+        mode: 'dark',
+    },
 });
 darkTheme = createTheme(darkTheme, {
-  // 用augmentColor创建的自定义主题色
-  palette: {
-    primary: darkTheme.palette.augmentColor({
-      color: {
-        main: "#9286d1",
-      },
-      name: "wMain",
-    }),
-    wMain: darkTheme.palette.augmentColor({
-      color: {
-        main: "#9286d1",
-      },
-      name: "wMain",
-    }),
-  },
+    // 用augmentColor创建的自定义主题色
+    palette: {
+        primary: darkTheme.palette.augmentColor({
+            color: {
+                main: '#9286d1',
+            },
+            name: 'wMain',
+        }),
+        wMain: darkTheme.palette.augmentColor({
+            color: {
+                main: '#9286d1',
+            },
+            name: 'wMain',
+        }),
+    },
 });
 export { lightTheme, darkTheme };

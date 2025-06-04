f2aca71a595e776ce57643aed486241dbd81c46a
white4206 <1834893128@qq.com>
2025-06-05 01:04:48 +0800

    refactor: 重构代码格式

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 80,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    server: {
+        port: 80,
     },
-  },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });

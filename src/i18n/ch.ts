2818ff276d7984d6b959665feaa7f181a54d57e0
white4206 <1834893128@qq.com>
2025-06-05 01:08:42 +0800

    feat: 添加国际化功能、路由懒加载、预添加新页面、Header组件重命名
    desc:
    - Header组件重命名为NavBar组件
    - 导航条添加国际化菜单, 目前支持中文和英语
    - 路由跳转使用lazy加载
    - 预添加文章、下载、视频页面

---
+++ src/i18n/ch.ts
@@ -0,0 +1,10 @@
+export default {
+    translation: {
+        pages: {
+            home: '主页',
+            article: '文章',
+            download: '下载',
+            video: '视频',
+        },
+    },
+};

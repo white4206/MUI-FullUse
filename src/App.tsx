784fbb24ad2eb25af90c571d8a994aa839afcdaa
white4206 <1834893128@qq.com>
2025-06-04 18:20:44 +0800

    feat: 构建项目基本布局
    desc:
    - 分离layout为Header组件和Container显示路由单页面
    - Header组件AppBar取消使用属性传递背景色改用css
    - 新增home、other路由页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from '@/layout/Header';
+import { CssBaseline, Box } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { lightTheme, darkTheme } from '@/theme/index.ts';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '@/pages/home';
+import Other from '@/pages/other';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDark, setIsDark] = useState(false);
+  const theme = isDark ? darkTheme : lightTheme;
+  const [headerHeight, setHeaderHeight] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header props={{ isDark, setIsDark, setHeaderHeight }} />
+      <Box id="container" p={2} mt={`${headerHeight}px`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/other" element={<Other />} />
+        </Routes>
+      </Box>
+    </ThemeProvider>
   )
 }
 

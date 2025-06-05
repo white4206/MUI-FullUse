40861130fd9f34f1e20cf4bc982f920ed34b20f4
white4206 <1834893128@qq.com>
2025-06-06 00:00:15 +0800

    fix: i18n菜单项采用传递数组控制、提取切换语言方法
    desc:
    - 将静态i18n菜单项转为由数组传递控制, 便于后续添加更多国际化支持
    - 提取切换语言并持久化用户首选项为通用方法

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,16 +2,30 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from '@/i18n/en';
 import ch from '@/i18n/ch';
+import chSvg from '@/assets/svg/ch.svg';
+import enSvg from '@/assets/svg/en.svg';
+
+const languages = [
+    { id: 1, language: 'ch', label: '简体中文', icon: chSvg },
+    { id: 2, language: 'en', label: 'English', icon: enSvg },
+];
 
 const language =
     localStorage.getItem('i18n') === 'auto'
         ? navigator.language || navigator.languages[0] // 若i18n为auto则使用浏览器当前语言
         : localStorage.getItem('i18n') || navigator.language || navigator.languages[0];
 
+const changeLanguage = (language: string) => {
+    void i18n.changeLanguage(language);
+    // 若切换语言为浏览器当前语言则跟随浏览器变化
+    if (navigator.language || navigator.languages[0] === language) localStorage.setItem('i18n', 'auto');
+    else localStorage.setItem('i18n', language);
+};
+
 void i18n.use(initReactI18next).init({
     resources: {
-        en,
         ch,
+        en,
     },
     lng: language || 'ch', // 默认语言
     fallbackLng: 'ch', // 回退默认语言
@@ -20,4 +34,4 @@ void i18n.use(initReactI18next).init({
     },
 });
 
-export default i18n;
+export { languages, changeLanguage };

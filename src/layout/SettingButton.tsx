7af927b5a80e7dde0d3c9b7e4d79b7d95e1cc87b
white4206 <1834893128@qq.com>
2025-06-08 14:49:00 +0800

    fix: 修复i18n切换后页面未立即渲染问题
    desc:
    - 应使用i18n的react钩子函数，这样才会在切换后重新渲染更新后的内容

--- src/layout/SettingButton.tsx
+++ src/layout/SettingButton.tsx
@@ -2,22 +2,23 @@ import { useBreakpoint } from '@/utils/hook';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { Divider, IconButton, Stack, SwipeableDrawer, Typography, useTheme } from '@mui/material';
 import { useState } from 'react';
-import { t } from 'i18next';
 import CloseIcon from '@mui/icons-material/Close';
+import { useTranslation } from 'react-i18next';
 
 const SettingButton = () => {
     const [open, setOpen] = useState(false);
     const { sm } = useBreakpoint();
     const theme = useTheme();
+    const { t } = useTranslation();
 
     return (
         <>
-            <IconButton sx={{ m: 0.5, borderRadius: 2 }} onClick={() => setOpen(true)}>
+            <IconButton sx={{ borderRadius: 2 }} onClick={() => setOpen(true)}>
                 <SettingsIcon />
             </IconButton>
             <SwipeableDrawer anchor="right" open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
-                <Stack direction="row" justifyContent="space-between" alignItems="center" p={2} width={sm ? 300 : 360}>
-                    <Typography>{t('navBar.setting')}</Typography>
+                <Stack direction="row" justifyContent="space-between" alignItems="center" p={2} width={sm ? 280 : 360}>
+                    <Typography fontWeight={500}>{t('navBar.setting')}</Typography>
                     <IconButton>
                         <CloseIcon sx={{ color: theme.palette.fullUseMain.main }} />
                     </IconButton>

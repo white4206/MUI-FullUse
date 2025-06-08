f808edd3e73e6f1c49f880dced69aa710f5ddeb1
white4206 <1834893128@qq.com>
2025-06-08 14:43:24 +0800

    style: 修改css样式逻辑
    desc:
    - 将系统css变量定义在theme.css中、主题及系统样式修改优先定义在其中
    - index.css修改原生样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/index.css';
+import '@/theme/index.css';
 import App from '@/App.tsx';
 import '@/i18n'; // i18国际化
 import { BrowserRouter } from 'react-router-dom';

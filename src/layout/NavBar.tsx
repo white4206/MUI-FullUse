c8be32b35293f04834e947df3b1365400c12742d
white4206 <1834893128@qq.com>
2025-06-09 23:37:50 +0800

    fix: 重写useDark切换黑暗模式自定义钩子, 修复切换主题时主题更新失败的问题
    desc:
    - 使用useMemo代替useState+useEffect更新theme, 使其在依赖发生变化时同步更新而非异步更新
    - 主题模式变量名由theme更改为themeMode
    - 将带动画切换主题提取到自定义钩子返回的方法中
    - 切换主题以及带动画的切换主题支持指定目标主题
    - 用户首选项store新增切换导航条按钮显示状态管理

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, IconButton, CardMedia, Box, Typography, useTheme, Divi
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import logo from '@/assets/svg/logo.svg';
-import React, { useRef, useLayoutEffect } from 'react';
+import { useRef, useLayoutEffect } from 'react';
 import I18nButton from '@/layout/I18nButton';
 import FontButton from '@/layout/FontButton';
 import SearchButton from '@/layout/SearchButton';
@@ -10,56 +10,19 @@ import SettingButton from '@/layout/SettingButton';
 import NotificationButton from '@/layout/NotificationButton';
 import Menu from '@/layout/Menu';
 import { useUserPreference } from '@/store';
+import { useDark } from '@/utils/hook';
 
 interface NavBarProps {
-    isDark: boolean;
-    toggleTheme: () => void;
     navBarHeight: number;
     setNavBarHeight: (clientHeight: number) => void;
 }
 
 const NavBar = ({ props }: { props: NavBarProps }) => {
-    const { isDark, toggleTheme, navBarHeight, setNavBarHeight } = props;
+    const { navBarHeight, setNavBarHeight } = props;
     const appBarRef = useRef<HTMLElement>(null);
     const theme = useTheme();
     const navBarButtons = useUserPreference(state => state.navBarButtons);
-
-    // 主题切换
-    const handleChangeTheme = async (e: React.MouseEvent) => {
-        /**
-         * 因为在 React 中, setIsDark(!isDark) 是异步的, 状态不会立刻更新.
-         * 需要用到切换后的值, 可以提前计算.
-         */
-        const nextIsDark = !isDark;
-        if (!document.startViewTransition) {
-            toggleTheme();
-        } else {
-            // 获取到 transition API 实例
-            const transition = document.startViewTransition(() => {
-                toggleTheme();
-            });
-            // 在 transition.ready 的 Promise 完成后，执行自定义动画
-            await transition.ready;
-            // 由于我们要从鼠标点击的位置开始做动画，所以我们需要先获取到鼠标的位置
-            const { clientX, clientY } = e;
-
-            // 计算半径，以鼠标点击的位置为圆心，到四个角的距离中最大的那个作为半径
-            const radius = Math.hypot(Math.max(clientX, innerWidth - clientX), Math.max(clientY, innerHeight - clientY));
-            const clipPath = [`circle(0% at ${clientX}px ${clientY}px)`, `circle(${radius}px at ${clientX}px ${clientY}px)`];
-            // 自定义动画
-            document.documentElement.animate(
-                {
-                    // 如果要切换到暗色主题，我们在过度的时候从半径 100% 的圆开始，到 0% 的圆结束
-                    clipPath: nextIsDark ? clipPath.reverse() : clipPath,
-                },
-                {
-                    duration: 500,
-                    // 如果要切换到暗色主题，我们应该裁剪 view-transition-old(root) 的内容
-                    pseudoElement: nextIsDark ? '::view-transition-old(root)' : '::view-transition-new(root)',
-                }
-            );
-        }
-    };
+    const { isDark, toggleThemeWithAnimation } = useDark();
 
     useLayoutEffect(() => {
         const updateHeight = () => {
@@ -92,11 +55,10 @@ const NavBar = ({ props }: { props: NavBarProps }) => {
                 {/* 搜索 */}
                 <SearchButton />
                 {/* 功能按钮 */}
-
                 <Box display={{ md: 'block', xs: 'none' }}>
                     {/* 主题切换按钮 */}
                     {navBarButtons.theme === 'navBar' && (
-                        <IconButton sx={{ m: 0.5, borderRadius: 2 }} onClick={e => void handleChangeTheme(e)}>
+                        <IconButton sx={{ borderRadius: 2 }} onClick={e => void toggleThemeWithAnimation(e)}>
                             {isDark ? <LightModeIcon /> : <DarkModeOutlinedIcon />}
                         </IconButton>
                     )}

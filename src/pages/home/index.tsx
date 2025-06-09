0eb0eb2a1669f673d61763646924db7a03ca1aee
white4206 <1834893128@qq.com>
2025-06-09 23:37:50 +0800

    fix: 重写useDark切换黑暗模式自定义钩子, 修复切换主题时主题更新失败的问题
    desc:
    - 使用useMemo代替useState+useEffect更新theme, 使其在依赖发生变化时同步更新而非异步更新
    - 主题模式变量名由theme更改为themeMode
    - 将带动画切换主题提取到自定义钩子返回的方法中
    - 切换主题以及带动画的切换主题支持指定目标主题
    - 用户首选项store新增切换导航条按钮显示状态管理

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,12 +3,12 @@ import { Box, Button } from '@mui/material';
 import { useShallow } from 'zustand/shallow';
 
 const Home = () => {
-    const [theme, language, font] = useUserPreference(useShallow(state => [state.theme, state.language, state.font]));
+    const [themeMode, language, font] = useUserPreference(useShallow(state => [state.themeMode, state.language, state.font]));
 
     return (
         <>
             <Box>当前用户首选项</Box>
-            <Box>{theme}</Box>
+            <Box>{themeMode}</Box>
             <Box>{language}</Box>
             <Box>{font}</Box>
         </>

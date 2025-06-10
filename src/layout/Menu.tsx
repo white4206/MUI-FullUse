e2fb568eba3fed0b1a9c5f797ba79f2d5cf39cd1
white4206 <1834893128@qq.com>
2025-06-10 21:58:25 +0800

    style: 导航条菜单、搜索按钮、设置菜单样式美化

--- src/layout/Menu.tsx
+++ src/layout/Menu.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography, useTheme, SwipeableDrawer, IconButton, CardMedia, Stack, Divider } from '@mui/material';
+import { Box, Button, Typography, useTheme, SwipeableDrawer, IconButton, CardMedia, Stack, Divider, Paper } from '@mui/material';
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -8,8 +8,9 @@ import HomeIcon from '@mui/icons-material/Home';
 import ArticleIcon from '@mui/icons-material/Article';
 import DownloadIcon from '@mui/icons-material/Download';
 import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import { useBreakpoint, useDark } from '@/utils/hook';
 
-const Menu = ({ props }: { props: { navBarHeight: number } }) => {
+const Menu = () => {
     const routeLinks = [
         { id: 1, title: 'pages.home', path: '/', icon: <HomeIcon /> },
         { id: 2, title: 'pages.article', path: '/article', icon: <ArticleIcon /> },
@@ -20,37 +21,43 @@ const Menu = ({ props }: { props: { navBarHeight: number } }) => {
     const [open, setOpen] = useState(false);
     const { t } = useTranslation();
     const currentPath = useLocation().pathname;
-    const { navBarHeight } = { ...props };
+    const { sm } = useBreakpoint();
+    const { isDark } = useDark();
 
     return (
         <>
-            <Stack direction="row" alignItems="center" display={{ lg: 'flex', xs: 'none' }} ml={1.5} mr={1.5}>
-                {routeLinks.map(routeLink => {
-                    return (
-                        <Box
-                            key={routeLink.id}
-                            className={currentPath === routeLink.path ? 'active-horizontal-menu-item' : ''}
-                            sx={{
-                                height: navBarHeight - 8,
-                                display: 'flex',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <Button sx={{ m: 0.5 }} component={Link} to={routeLink.path}>
-                                <Typography color={theme.palette.text.primary}>{t(routeLink.title)}</Typography>
-                            </Button>
-                        </Box>
-                    );
-                })}
-            </Stack>
+            <Paper variant="outlined" sx={{ display: { lg: 'flex', xs: 'none' }, borderRadius: 8, ml: 1, mr: 1 }}>
+                <Stack direction="row" alignItems="center">
+                    {routeLinks.map(routeLink => {
+                        return (
+                            <Box
+                                mt={0.5}
+                                mb={0.5}
+                                ml={1}
+                                mr={1}
+                                display="flex"
+                                key={routeLink.id}
+                                alignItems="center"
+                                className={currentPath === routeLink.path ? 'active-horizontal-menu-item' : ''}
+                            >
+                                <Button sx={{ borderRadius: 8 }} size="large" component={Link} to={routeLink.path}>
+                                    <Typography color={isDark ? theme.palette.fullUseMain.light : theme.palette.fullUseMain.dark}>
+                                        {t(routeLink.title)}
+                                    </Typography>
+                                </Button>
+                            </Box>
+                        );
+                    })}
+                </Stack>
+            </Paper>
             <IconButton sx={{ m: 0.5, borderRadius: 2, display: { lg: 'none', xs: 'flex' } }} onClick={() => setOpen(true)}>
                 <MenuIcon sx={{ color: theme.palette.fullUseMain.main }} />
             </IconButton>
             <SwipeableDrawer anchor="left" open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
-                <Stack direction="row" alignItems="center" p={2} width={240}>
+                <Stack direction="row" alignItems="center" p={2} width={sm ? 190 : 240}>
                     {/* logo */}
                     <a href="http://resource.whitecc.top" target="_blank" rel="noreferrer noopener">
-                        <CardMedia component={'img'} sx={{ width: 32 }} image={logo} />
+                        <CardMedia component={'img'} sx={{ width: 32, transition: '.4s', '&:hover': { transform: 'rotate(180deg)' } }} image={logo} />
                     </a>
                     <Divider variant="middle" orientation="vertical" flexItem sx={{ m: 1, ml: 1, mr: 0 }} />
                     <Typography p={1} color={theme.palette.text.primary}>

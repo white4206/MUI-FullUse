7ddf42f54c6112dfcfaf8cd1d56f7eb3a8280428
white4206 <1834893128@qq.com>
2025-06-11 15:02:27 +0800

    fix: 修复useBreakPoint中屏幕尺寸断点判断对应错误、article、video页面添加demo内容

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -51,7 +51,7 @@ const NavBar = ({ props }: { props: NavBarProps }) => {
             <Toolbar>
                 {/* logo */}
                 <a href="http://resource.whitecc.top" target="_blank" rel="noreferrer noopener">
-                    <CardMedia component={'img'} sx={{ width: 32, transition: '.4s', '&:hover': { transform: 'rotate(180deg)' } }} image={logo} />
+                    <CardMedia component="img" sx={{ width: 32, transition: '.4s', '&:hover': { transform: 'rotate(180deg)' } }} image={logo} />
                 </a>
                 <Divider variant="middle" orientation="vertical" flexItem sx={{ m: 2.5, ml: 1, mr: 0, display: { sm: 'block', xs: 'none' } }} />
                 <Typography display={{ sm: 'block', xs: 'none' }} p={1} color={theme.palette.text.primary}>

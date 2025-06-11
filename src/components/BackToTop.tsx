f8fbe823fb39fc1edf21631f3b6c20a7339648a8
white4206 <1834893128@qq.com>
2025-06-11 10:28:13 +0800

    feat: 新增回到顶部功能
    desc:
    - 通用组件BackToTop, 支持传递设置滚动元素, 固定位置和显示高度
    - 适配BackToTop按钮样式

---
+++ src/components/BackToTop.tsx
@@ -0,0 +1,40 @@
+import { useDark } from '@/utils/hook';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { IconButton, Box, useScrollTrigger, useTheme, Tooltip } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+interface BackToTopProps {
+    target?: HTMLElement | null;
+    visibilityHeight?: number;
+    right?: number;
+    bottom?: number;
+}
+
+const BackToTop = (props: BackToTopProps) => {
+    const { target, visibilityHeight = 200, right = 32, bottom = 32 } = props;
+    const theme = useTheme();
+    const { t } = useTranslation();
+    const { isDark } = useDark();
+    const trigger = useScrollTrigger({
+        threshold: visibilityHeight, // 滚动超过100px时trigger为true
+        disableHysteresis: true, // 立即响应，不延迟});
+    });
+
+    const handleBackToTop = () => {
+        const scrollTarget = target || document.documentElement;
+        scrollTarget.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    return (
+        trigger && (
+            <Tooltip title={t('backToTop')} placement="left" enterDelay={500}>
+                <Box position="fixed" right={right} bottom={bottom}>
+                    <IconButton className="back-to-top-button" color="primary" onClick={handleBackToTop}>
+                        <KeyboardArrowUpIcon sx={{ color: isDark ? theme.palette.fullUseMain.light : theme.palette.fullUseMain.dark }} />
+                    </IconButton>
+                </Box>
+            </Tooltip>
+        )
+    );
+};
+export default BackToTop;

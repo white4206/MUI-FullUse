6da5ef346e9674e011e7bad309d33a773a3c7935
white4206 <1834893128@qq.com>
2025-06-11 17:07:29 +0800

    feat: 添加vite-plugin-svg-icons插件, svg文件使用SvgIcon组件展示
    desc:
    - 添加vite-plugin-svg-icons依赖并配置vite
    - 在main.tsx入口文件中注册插件并手动配置其相应的类型声明
    - 新增SvgIcon组件, 使用iconName动态使用svg图标
    - 替换使用原使用svg图标的img直接使用SvgIcon

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import 'swiper/swiper-bundle.css';
 import '@/theme/index.css';
 import '@/i18n'; // i18国际化
 import { BrowserRouter } from 'react-router-dom';
+import 'virtual:svg-icons-register';
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>

aa65b7f2054da8b043cdb355ba6af0372668ac2e
white4206 <1834893128@qq.com>
2025-07-08 17:30:12 +0800

    feat: 新增Notification组件
    desc:
    - 封装Snackbar和Alert组件为Notification组件用于通知用户操作

---
+++ src/components/Notification.tsx
@@ -0,0 +1,35 @@
+import { Alert, Snackbar, Zoom, type AlertColor, type AlertProps, type SnackbarOrigin, type SnackbarProps } from '@mui/material';
+
+interface NotificationProps {
+    open: boolean;
+    option?: { snackbarProps: SnackbarProps; alertProps: AlertProps };
+    content: string;
+    type?: AlertColor;
+    variant?: 'standard' | 'filled' | 'outlined';
+    position?: SnackbarOrigin;
+    autoHideDuration?: number;
+    onClose?: (event: React.SyntheticEvent | Event, reason: string) => void;
+}
+
+const defaultPosition: SnackbarOrigin = { vertical: 'top', horizontal: 'right' };
+
+const Notification = (props: NotificationProps) => {
+    const { open, position = defaultPosition, autoHideDuration = 3000, onClose, option, type = 'success', variant = 'standard', content } = props;
+
+    return (
+        <Snackbar
+            {...(option?.snackbarProps || {})}
+            slots={{ transition: Zoom }}
+            open={open}
+            anchorOrigin={position}
+            autoHideDuration={autoHideDuration}
+            onClose={onClose}
+        >
+            <Alert {...(option?.alertProps || {})} severity={type} variant={variant}>
+                {content}
+            </Alert>
+        </Snackbar>
+    );
+};
+
+export default Notification;

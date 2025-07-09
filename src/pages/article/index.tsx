e42268f56987afc15e7d92b6a44392c6c1079935
white4206 <1834893128@qq.com>
2025-07-09 23:10:51 +0800

    style: 新增一些MUI demo

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -7,6 +7,7 @@ import Typography from '@mui/material/Typography';
 import IconSelect from '@/components/IconSelect';
 import CustomInput from '@/components/CustomInput';
 import { useNotification } from '@/context/hooks';
+import { Box } from '@mui/material';
 
 const Article = () => {
     const handleSelectIcon = (icon: string) => {
@@ -15,8 +16,8 @@ const Article = () => {
     const notify = useNotification();
 
     return (
-        <>
-            <Card sx={{ maxWidth: 345 }}>
+        <Box overflow="hidden">
+            <Card sx={{ maxWidth: 345, m: 2 }}>
                 <CardMedia sx={{ height: 140 }} image="https://mui.com/static/images/cards/contemplative-reptile.jpg" title="green iguana" />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
@@ -31,10 +32,12 @@ const Article = () => {
                     <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
-            <CustomInput label="CustomInput" />
+            <CustomInput sx={{ m: 2 }} label="自定义输入框" />
             <IconSelect sx={{ m: 2 }} onIconSelect={handleSelectIcon} />
-            <Button onClick={() => notify({ content: '我是通知' })}>通知</Button>
-        </>
+            <Button sx={{ m: 2 }} onClick={() => notify({ content: '我是通知' })}>
+                通知
+            </Button>
+        </Box>
     );
 };
 export default Article;

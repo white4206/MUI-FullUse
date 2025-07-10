95f6a42bcb9f241eefa9ca966e6227802bec53d3
white4206 <1834893128@qq.com>
2025-07-10 10:08:25 +0800

    feat: 工具箱添加后端交互

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,9 @@
 const mapUrl = (url: string | null, failureUrl = null) => {
     return url ? new URL(import.meta.env.VITE_APP_BASE_API + url, import.meta.url).href : failureUrl;
 };
-export { mapUrl };
+
+const openUrl = (url: string, external = true): void => {
+    window.open(external ? url : location.href.split('/')[0] + url);
+};
+
+export { mapUrl, openUrl };

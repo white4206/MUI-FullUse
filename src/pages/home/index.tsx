40a36a48e03e46b018090152deece51bf2a8a162
white4206 <1834893128@qq.com>
2025-07-13 02:09:04 +0800

    feat: 新增FUTextField组件、弃用CustomInput组件
    desc:
    - 新增inputBgColor主题色, 重构部分主题变量名称、使用rgba表达颜色
    - 新增FUTextField组件重写MUI TextField组件样式调整部分逻辑
    - 弃用CustomInput组件替换为FUTextField组件
    - 调整部分样式

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,63 +1,20 @@
-import {
-    Box,
-    Card,
-    CardActionArea,
-    CardContent,
-    CardMedia,
-    CircularProgress,
-    Container,
-    Grid,
-    IconButton,
-    Paper,
-    Skeleton,
-    Stack,
-    Tooltip,
-    Typography,
-    useTheme,
-} from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, CardMedia, CircularProgress, Container, Grid, Skeleton, Stack, Typography, useTheme } from '@mui/material';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import { useBreakpoint } from '@/utils/hooks';
-import HandymanIcon from '@mui/icons-material/Handyman';
-import { useTranslation } from 'react-i18next';
-import SvgIcon from '@/components/SvgIcon';
 import { useEffect, useState } from 'react';
-import { getCarousel, getToolOption } from '@/api/common';
-import { mapUrl, openUrl } from '@/utils/url';
+import { getCarousel } from '@/api/common';
+import { mapUrl } from '@/utils/url';
 import { getHeadArticle } from '@/api/article';
-import React from 'react';
-
-const ToolBoxItem = ({ data }: { data: ToolOption }) => {
-    const theme = useTheme();
-
-    return (
-        <CardActionArea sx={{ borderRadius: 2, flex: '0 0 25%' }}>
-            <Stack
-                direction="column"
-                alignItems="center"
-                justifyContent="center"
-                borderRadius={2}
-                p={1}
-                sx={{ transition: '.4s', cursor: 'pointer', '&:hover': { bgcolor: theme.palette.action.hover } }}
-                onClick={() => openUrl(data.path)}
-            >
-                <SvgIcon iconName={data.icon} size="32px" />
-                <Typography variant="body2">{data.name}</Typography>
-            </Stack>
-        </CardActionArea>
-    );
-};
+import Toolbox from '@/pages/home/Toolbox';
 
 const Home = () => {
     const theme = useTheme();
     const { xl, lg, md, sm } = useBreakpoint();
-    const { t } = useTranslation();
     const [isSwiperDataLoading, setIsSwiperDataLoading] = useState<boolean>(true);
     const [swiperData, setSwiperData] = useState<Carousel[]>([]);
     const [isHeadArticleLoading, setIsHeadArticleLoading] = useState<boolean>(true);
     const [headArticleData, setHeadArticleData] = useState<HeadArticle | null>(null);
-    const [isToolOptionLoading, setIsToolOptionLoading] = useState<boolean>(true);
-    const [toolOptionData, setToolOptionData] = useState<ToolOption[] | null>(null);
 
     useEffect(() => {
         // * 获取轮播图数据
@@ -81,122 +38,89 @@ const Home = () => {
             setIsHeadArticleLoading(false);
         };
         void getHeadArticleData();
-        // * 获取工具项
-        const getToolOptionData = async () => {
-            const res = await getToolOption();
-            setToolOptionData(res.data);
-            setIsToolOptionLoading(false);
-        };
-        void getToolOptionData();
     }, []);
 
     return (
-        <Box bgcolor={theme.palette.bgColor}>
+        <>
+            <Box bgcolor={theme.palette.bgColor}>
+                <Container maxWidth="xl" sx={{ p: 2, pl: { xl: 10, lg: 10 }, pr: { xl: 10, lg: 10 } }}>
+                    <Grid justifyContent="center" container spacing={xl ? 8 : lg ? 6 : 0}>
+                        {/* 左侧轮播图 */}
+                        <Grid size={{ xs: 12, sm: 11, md: 10, lg: 8, xl: 8 }} sx={{ position: 'relative' }}>
+                            {isSwiperDataLoading ? (
+                                <>
+                                    <Skeleton sx={{ borderRadius: 4 }} variant="rounded" height={xl ? 500 : lg ? 450 : md ? 400 : sm ? 300 : 200} />
+                                    <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}>
+                                        <CircularProgress color="primary" />
+                                    </Box>
+                                </>
+                            ) : (
+                                <Swiper
+                                    style={{ borderRadius: 16 }}
+                                    modules={[Pagination]}
+                                    pagination={{ clickable: true, dynamicBullets: true }}
+                                    autoplay={{ pauseOnMouseEnter: true }}
+                                    loop
+                                >
+                                    {swiperData.map(swiper => {
+                                        return (
+                                            <SwiperSlide key={swiper.id}>
+                                                <CardMedia
+                                                    height={xl ? 500 : lg ? 450 : md ? 400 : sm ? 300 : 200}
+                                                    component="img"
+                                                    image={swiper.image || undefined}
+                                                />
+                                            </SwiperSlide>
+                                        );
+                                    })}
+                                </Swiper>
+                            )}
+                        </Grid>
+                        {/* 右侧头文章+工具箱 */}
+                        <Grid display={{ xs: 'none', lg: 'block' }} size={{ xs: 0, sm: 0, md: 0, lg: 4, xl: 4 }}>
+                            <Stack height="100%" direction="column" justifyContent="space-between">
+                                {/* 头文章 */}
+                                <Card sx={{ borderRadius: 4 }} elevation={3}>
+                                    {isHeadArticleLoading ? (
+                                        <>
+                                            <Skeleton height={xl ? 180 : 130} sx={{ borderRadius: 4 }} variant="rounded" />
+                                            <CardContent>
+                                                <Skeleton height={16} sx={{ mb: 1, width: '70%' }} variant="rounded" />
+                                                <Skeleton height={16} sx={{ mb: 1 }} variant="rounded" />
+                                            </CardContent>
+                                        </>
+                                    ) : (
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                sx={{ height: { xl: 180, lg: 130 }, borderRadius: 4 }}
+                                                image={headArticleData?.cover || undefined}
+                                            />
+                                            <CardContent>
+                                                <Typography className="text-ellipsis-2" gutterBottom variant="subtitle1" fontWeight={500}>
+                                                    {headArticleData?.title}
+                                                </Typography>
+                                                <Typography variant="body2" sx={{ color: theme.palette.text.secondary }} className="text-ellipsis-2">
+                                                    {headArticleData?.abstractText}
+                                                </Typography>
+                                            </CardContent>
+                                        </CardActionArea>
+                                    )}
+                                </Card>
+                                {/* 工具箱 */}
+                                <Toolbox />
+                            </Stack>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
             <Container maxWidth="xl" sx={{ p: 2, pl: { xl: 10, lg: 10 }, pr: { xl: 10, lg: 10 } }}>
                 <Grid justifyContent="center" container spacing={xl ? 8 : lg ? 6 : 0}>
-                    {/* 左侧轮播图 */}
-                    <Grid size={{ xs: 12, sm: 11, md: 10, lg: 8, xl: 8 }} sx={{ position: 'relative' }}>
-                        {isSwiperDataLoading ? (
-                            <>
-                                <Skeleton sx={{ borderRadius: 4 }} variant="rounded" height={xl ? 500 : lg ? 450 : md ? 400 : sm ? 300 : 200} />
-                                <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}>
-                                    <CircularProgress color="primary" />
-                                </Box>
-                            </>
-                        ) : (
-                            <Swiper
-                                style={{ borderRadius: 16 }}
-                                modules={[Pagination]}
-                                pagination={{ clickable: true, dynamicBullets: true }}
-                                autoplay={{ pauseOnMouseEnter: true }}
-                                loop
-                            >
-                                {swiperData.map(swiper => {
-                                    return (
-                                        <SwiperSlide key={swiper.id}>
-                                            <CardMedia
-                                                height={xl ? 500 : lg ? 450 : md ? 400 : sm ? 300 : 200}
-                                                component="img"
-                                                image={swiper.image || undefined}
-                                            />
-                                        </SwiperSlide>
-                                    );
-                                })}
-                            </Swiper>
-                        )}
-                    </Grid>
-                    {/* 右侧头文章+工具箱 */}
-                    <Grid display={{ xs: 'none', lg: 'block' }} size={{ xs: 0, sm: 0, md: 0, lg: 4, xl: 4 }}>
-                        <Stack height="100%" direction="column" justifyContent="space-between">
-                            {/* 头文章 */}
-                            <Card sx={{ borderRadius: 4 }} elevation={3}>
-                                {isHeadArticleLoading ? (
-                                    <>
-                                        <Skeleton height={xl ? 180 : 130} sx={{ borderRadius: 4 }} variant="rounded" />
-                                        <CardContent>
-                                            <Skeleton height={16} sx={{ mb: 1, width: '70%' }} variant="rounded" />
-                                            <Skeleton height={16} sx={{ mb: 1 }} variant="rounded" />
-                                        </CardContent>
-                                    </>
-                                ) : (
-                                    <CardActionArea>
-                                        <CardMedia sx={{ height: { xl: 180, lg: 130 }, borderRadius: 4 }} image={headArticleData?.cover || undefined} />
-                                        <CardContent>
-                                            <Typography className="text-ellipsis-2" gutterBottom variant="subtitle1" fontWeight={500}>
-                                                {headArticleData?.title}
-                                            </Typography>
-                                            <Typography variant="body2" sx={{ color: theme.palette.text.secondary }} className="text-ellipsis-2">
-                                                {headArticleData?.abstractText}
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                )}
-                            </Card>
-                            {/* 工具箱 */}
-                            <Card sx={{ borderRadius: 4 }} elevation={3}>
-                                <CardContent sx={{ p: '0 !important', bgcolor: theme.palette.toolboxBgColor }}>
-                                    <Stack direction="row" alignItems="center" p={2} pb={1}>
-                                        <Typography fontWeight={600} variant="h6">
-                                            {t('pages.home.toolbox.title')}
-                                        </Typography>
-                                        <Tooltip title={t('pages.home.toolbox.edit.title')} placement="right">
-                                            <IconButton
-                                                onClick={() => {}}
-                                                sx={{ borderRadius: 2, ml: 1, transition: '.4s', '&:hover': { transform: 'rotate(15deg)' } }}
-                                            >
-                                                <HandymanIcon sx={{ fontSize: 14, color: theme.palette.fullUseMain.main }} />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </Stack>
-                                    <Paper sx={{ borderRadius: 4 }} elevation={3}>
-                                        {isToolOptionLoading ? (
-                                            <Stack direction="row" justifyContent="space-around" p={1}>
-                                                {[1, 2, 3, 4].map(item => {
-                                                    return (
-                                                        <React.Fragment key={item}>
-                                                            <Stack direction="column" alignItems="center" spacing={1} p={1} m={1}>
-                                                                <Skeleton height={32} width={32} sx={{ borderRadius: 2 }} variant="rounded" />
-                                                                <Skeleton height={12} width={32} variant="rounded" />
-                                                            </Stack>
-                                                        </React.Fragment>
-                                                    );
-                                                })}
-                                            </Stack>
-                                        ) : (
-                                            <Stack direction="row" p={1} sx={{ overflowX: 'auto' }}>
-                                                {toolOptionData?.map(item => {
-                                                    return <ToolBoxItem key={item.id} data={item} />;
-                                                })}
-                                            </Stack>
-                                        )}
-                                    </Paper>
-                                </CardContent>
-                            </Card>
-                        </Stack>
-                    </Grid>
+                    <Grid size={{ xs: 12, sm: 11, md: 10, lg: 8, xl: 8 }} sx={{ position: 'relative' }}></Grid>
+                    <Grid display={{ xs: 'none', lg: 'block' }} size={{ xs: 0, sm: 0, md: 0, lg: 4, xl: 4 }}></Grid>
                 </Grid>
             </Container>
-        </Box>
+        </>
     );
 };
 

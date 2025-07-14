ff92c7f91bbdf3576cf73572822ce7c6304d3fd9
white4206 <1834893128@qq.com>
2025-07-14 22:26:40 +0800

    feat: 新增SideCard组件、新增首页相关链接卡片板块并添加交换
    desc:
    - 使用MUI Card组件封装通用侧边卡片组件
    - 新增首页相关链接卡片板块
    - 相关链接卡片添加后端交换

--- src/api/types/common.d.ts
+++ src/api/types/common.d.ts
@@ -1,11 +1,19 @@
 interface Carousel {
     id: number;
     title: string;
-    image: string | null;
+    image: string;
 }
 interface ToolOption {
     id: number;
     icon: string;
     name: string;
     path: string;
+}
+interface RelatedLink {
+    id: number;
+    icon: string;
+    title: string;
+    url: string;
+    sideIcon?: string;
+    subLinks?: Omit<RelatedLink, 'subLinks'>[];
 }

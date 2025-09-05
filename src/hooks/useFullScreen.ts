17b1c2cb64aa6b1cbb9872ed36096a4434d392ac
white4206 <1834893128@qq.com>
2025-09-05 17:13:12 +0800

    wip: ArticleItem组件work in process

--- src/hooks/useFullScreen.ts
+++ src/hooks/useFullScreen.ts
@@ -1,5 +1,8 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import screenfull from 'screenfull';
+import { debounce } from 'lodash';
+import 'numeral/locales';
+import numeral from 'numeral';
 
 const useFullScreen = (target = document.documentElement) => {
     const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
@@ -49,3 +52,68 @@ const useFullScreen = (target = document.documentElement) => {
 };
 
 export default useFullScreen;
+const useBreakpoint = () => {
+    /**
+     * xs, extra-small: 0px
+     * sm, small: 600px
+     * md, medium: 900px
+     * lg, large: 1200px
+     * xl, extra-large: 1536px
+     */
+    const xs = useMediaQuery(theme => theme.breakpoints.between('xs', 'sm'));
+    const sm = useMediaQuery(theme => theme.breakpoints.between('sm', 'md'));
+    const md = useMediaQuery(theme => theme.breakpoints.between('md', 'lg'));
+    const lg = useMediaQuery(theme => theme.breakpoints.between('lg', 'xl'));
+    const xl = useMediaQuery(theme => theme.breakpoints.up('xl'));
+    return { xs, sm, md, lg, xl };
+};
+
+const useDebounce = <T extends (...args: any[]) => any>(callback: T, delay: number) => {
+    const debouncedCallback = useCallback(debounce(callback, delay), [callback, delay]);
+
+    return debouncedCallback;
+};
+
+const useNumeral = () => {
+    const { language } = useUserPreference();
+    numeral.zeroFormat('N/A');
+    numeral.defaultFormat('0,0.0a');
+
+    useEffect(() => {
+        // languages.forEach(language => {
+        //     if (language.language === 'en') return;
+        //     numeral.register('locale', language.language, {
+        //         delimiters: {
+        //             thousands: ' ',
+        //             decimal: ',',
+        //         },
+        //         abbreviations: {
+        //             thousand: 'k',
+        //             million: 'm',
+        //             billion: 'b',
+        //             trillion: 't',
+        //         },
+        //         ordinal: function (number) {
+        //             return number === 1 ? 'er' : 'ème';
+        //         },
+        //         currency: {
+        //             symbol: '€',
+        //         },
+        //     });
+        // });
+    }, []);
+
+    useEffect(() => {
+        const languageLocaleMap: Record<string, string> = {
+            'zh-CN': 'chs',
+            en: 'en',
+        };
+        const currentLanguage = language === 'auto' ? navigator.language || navigator.languages[0] : language;
+        const currentLocale = languageLocaleMap[currentLanguage] || 'en';
+        numeral.locale(currentLocale);
+    }, [language]);
+
+    return numeral;
+};
+
+export { useDark, useFullScreen, useBreakpoint, useDebounce, useNumeral };
